fff922d79dcc9dd0df44c31d51ad62565a8110a3
Default <s.gonzalezpons@gmail.com>
2020-01-03 13:58:33 +0100

    Cameras adapted to gamemodes

---
+++ Assets/Scripts/BasePart/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject Player;
+
+    public float turnSpeed = 4.0f;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(4.0f, 19.0f, 35.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = Player.transform.position + offset;
+        transform.LookAt(Player.transform.position);
+    }
+}
---
+++ Assets/Scripts/BasePart/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject Cam;
+
+    void Update()
+    {
+        if (Cam.activeSelf == true)
+        {
+
+        }
+    }
+}
