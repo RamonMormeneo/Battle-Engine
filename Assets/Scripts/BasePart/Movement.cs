5ca15c986a7503574177f76169d36f731b85d47d
Default <s.gonzalezpons@gmail.com>
2020-01-03 19:03:37 +0100

    Movement and camera rotation

--- Assets/Scripts/BasePart/Movement.cs
+++ Assets/Scripts/BasePart/Movement.cs
@@ -6,11 +6,49 @@ public class Movement : MonoBehaviour
 {
     public GameObject Cam;
 
+    public Vector3 move;
+    public Vector3 velocity; 
+    public float maxSpeed = 35.0f; 
+    public float acceleration = 5.0f; 
+    public float brake = 5.0f; 
+    public float turnSpeed = 45.0f;
+    private float speed = 0.0f;
+
     void Update()
     {
         if (Cam.activeSelf == true)
         {
+            float turn = Input.GetAxis("Horizontal");
+            transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 
+
+            float forwards = Input.GetAxis("Vertical");
+
+            if (forwards > 0)
+            {
+                speed = speed + acceleration * Time.deltaTime;
+            }
+
+            else if (forwards < 0)
+            {
+                speed = speed - acceleration * Time.deltaTime;
+            }
+
+            else
+            {
+                if (speed > 0)
+                {
+                    speed = speed - brake * Time.deltaTime;
+                }
+                else
+                {
+                    speed = speed + brake * Time.deltaTime;
+                }
+            }
+
+            speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
+            Vector3 velocity = new Vector3(0.0f, 0.0f, speed);
+            transform.Translate(velocity * Time.deltaTime, Space.Self);
         }
     }
 }
