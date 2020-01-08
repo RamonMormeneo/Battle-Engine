6404a0745b1d3fced424c2f1a237875000400a1e
victorarmisen <victorarmisencapo@enti.cat>
2020-01-08 19:58:05 +0100

    Gancho

---
+++ Assets/Scripts/Gancho.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gancho : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+}
