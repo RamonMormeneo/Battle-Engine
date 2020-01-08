40010e8d55025b15c2f99ac9130fae576de30e1a
victorarmisen <victorarmisencapo@enti.cat>
2020-01-08 12:41:50 +0100

    Movimiento, cursor y para aceleracion al choque

--- Assets/Scripts/BasePart/CameraMovement.cs
+++ Assets/Scripts/BasePart/CameraMovement.cs
@@ -14,12 +14,13 @@ public class CameraMovement : NetworkBehaviour
     void Start()
     {
         offset = new Vector3(4.0f, 19.0f, 35.0f);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {      
         offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
         transform.position = Player.transform.position + offset;
         transform.LookAt(Player.transform.position);
--- Assets/Scripts/BasePart/Movement.cs
+++ Assets/Scripts/BasePart/Movement.cs
@@ -14,8 +14,15 @@ public class Movement : NetworkBehaviour
     public float brake = 5.0f; 
     public float turnSpeed = 45.0f;
     private float speed = 0.0f;
- 
-    void Update()
+    private Rigidbody body;
+    public float speed_multiply;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
     {
 
         if (!hasAuthority)
@@ -54,9 +61,16 @@ public class Movement : NetworkBehaviour
 
                 speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
                 Vector3 velocity = new Vector3(0.0f, 0.0f, speed);
-                transform.Translate(velocity * Time.deltaTime, Space.Self);
-            }
+                //transform.Translate(velocity * Time.deltaTime, Space.Self); // Old movement
+                body.MovePosition(transform.position + transform.forward * speed * Time.deltaTime * speed_multiply); // New movement
+        }
         
        
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+    }
+
 }
